79695aa4d8aa0710f27962958f0fbc793d2555e8
Luis Maldonado <maldonadovla@gmail.com>
2020-10-16 19:22:39 -0500

    Add database import

--- src/api/v1/controllers/urls/index.js
+++ src/api/v1/controllers/urls/index.js
@@ -1,6 +1,18 @@
+import database from '../../../../db';
+
 async function getUrlController(req, res, next) {
   try {
-  } catch (error) {}
+    const url = database.get(req.param.url);
+
+    switch (url) {
+      case null:
+        res.status(404).send('URL does not exist');
+      default:
+        break;
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
 }
 
 export { getUrlController };
