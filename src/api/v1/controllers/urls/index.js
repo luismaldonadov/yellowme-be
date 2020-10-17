817e7afe2fda1e7b6e70276686356cd035ab5649
Luis Maldonado <maldonadovla@gmail.com>
2020-10-17 13:15:24 -0500

    Relocated entry point of app

--- src/api/v1/controllers/urls/index.js
+++ src/api/v1/controllers/urls/index.js
@@ -8,27 +8,14 @@ import validateUrl from './schema';
 async function getUrlController(req, res, next) {
   try {
     const urlParam = req.query.url;
-    if (!urlParam) {
-      getPagedUrlsController(req, res, next);
-    } else {
-      validateUrl(urlParam);
-      const url = database.get(urlParam);
-      switch (urlParam) {
-        case null:
-          return res.status(404).send({ shortUrl: null });
-        default:
-          return res.status(200).redirect(301, url);
-      }
+    validateUrl(urlParam);
+    const url = database.get(urlParam);
+    switch (urlParam) {
+      case null:
+        return res.status(404).send({ shortUrl: null });
+      default:
+        return res.status(200).redirect(301, url);
     }
-  } catch (error) {
-    return errorWrapper(res, error);
-  }
-}
-
-async function getPagedUrlsController(req, res, next) {
-  try {
-    //TODO: Paged urls
-    return res.send();
   } catch (error) {
     return errorWrapper(res, error);
   }
@@ -36,11 +23,13 @@ async function getPagedUrlsController(req, res, next) {
 
 async function createShortUrlController(req, res, next) {
   try {
-    const shortUrl = shortenUrl(req.body.url);
+    const longUrl = req.body.url;
+    validateUrl(longUrl);
+    const shortUrl = shortenUrl(longUrl);
     return res.send({ shortUrl });
   } catch (error) {
     return errorWrapper(res, error);
   }
 }
 
-export { getUrlController, getPagedUrlsController, createShortUrlController };
+export { getUrlController, createShortUrlController };
