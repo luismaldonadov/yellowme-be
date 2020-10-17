4c2b85ff221d324a13947cbb1ac2840c1d36ee11
Luis Maldonado <maldonadovla@gmail.com>
2020-10-17 15:27:27 -0500

    Change to commonjs on error wrapper

--- src/api/v1/controllers/urls/schema.js
+++ src/api/v1/controllers/urls/schema.js
@@ -1,4 +1,5 @@
-const Joi, { ValidationError } = require('joi');
+const { ValidationError } = require('joi');
+const Joi = require('joi');
 
 const urlRegex = /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$/;
 
