5a9f91065ca47a2002871926eba5f1d5ba59d862
Luis Maldonado <maldonadovla@gmail.com>
2020-10-17 13:37:49 -0500

    Add test for invalid format url

--- src/tests/integration/api/v1/controllers/urls/index.ispec.test.js
+++ src/tests/integration/api/v1/controllers/urls/index.ispec.test.js
@@ -6,8 +6,8 @@ import urlShortener from '../../../../../../components/urls';
 const apiBase = '/api/v1';
 
 describe('getUrlContoller', () => {
+  const endpoint = '/short_urls';
   test('with valid url param, returns 302', async () => {
-    const endpoint = '/short_urls';
     const longUrl = 'https://test1.com';
     const shortUrl = urlShortener(longUrl);
 
@@ -21,7 +21,6 @@ describe('getUrlContoller', () => {
   });
 
   test('with non existent url, returns 404', async () => {
-    const endpoint = '/short_urls';
     const unexistentShortUrl = 'https://test2.com';
 
     const { status, body } = await request(app)
@@ -30,5 +29,16 @@ describe('getUrlContoller', () => {
 
     expect(status).toBe(404);
     expect(body).toStrictEqual({ shortUrl: null });
+  });
+
+  test('with invalid url, returns 422', async () => {
+    const invalidUrl = 'https://test3';
+
+    const { status, body } = await request(app)
+      .get(`${apiBase}${endpoint}`)
+      .query({ url: invalidUrl });
+
+    expect(status).toBe(422);
+    expect(body).toStrictEqual({ error: 'ValidationError' });
   });
 });
