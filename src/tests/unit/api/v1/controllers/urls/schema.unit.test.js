0879ac2c1b12dafa4b2d5affedc2b382bf010adf
Luis Maldonado <maldonadovla@gmail.com>
2020-10-17 15:21:43 -0500

    Change to common js on schema unit test

--- src/tests/unit/api/v1/controllers/urls/schema.unit.test.js
+++ src/tests/unit/api/v1/controllers/urls/schema.unit.test.js
@@ -1,8 +1,8 @@
+const { ValidationError } = require('joi');
+
 const {
   validateUrl,
 } = require('../../../../../../api/v1/controllers/urls/schema');
-
-const { ValidationError } = require('joi');
 
 describe('test url schema ', () => {
   test('throws a Joi Validation error when passed a wrong url', () => {
