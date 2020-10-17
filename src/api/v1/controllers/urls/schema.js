4e73f1d9385e5e171bdfedf4f65bbb9f487f41de
Luis Maldonado <maldonadovla@gmail.com>
2020-10-17 15:21:06 -0500

    Change to common js on urls schema

--- src/api/v1/controllers/urls/schema.js
+++ src/api/v1/controllers/urls/schema.js
@@ -1,4 +1,4 @@
-import Joi, { ValidationError } from 'joi';
+const Joi, { ValidationError } = require('joi');
 
 const urlRegex = /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$/;
 
